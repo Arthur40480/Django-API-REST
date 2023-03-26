681a91378149de9be6c5b1c5b5e3f1f21fc14dcf
Arthur40480 <arthurgibert@free.fr>
2023-03-26 16:59:18 +0200

    Add action

--- shop/serializers.py
+++ shop/serializers.py
@@ -4,19 +4,22 @@ from rest_framework import serializers
 from shop.models import Category, Product, Article
 
 
-
-
-
 class ProductSerializer(serializers.ModelSerializer):
 
-    articles = serializers.SerializerMethodField()
+    class Meta:
+        model = Product
+        fields = ['id', 'date_created', 'date_updated', 'name', 'category']
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+
+    articles = serializers.SerializerMethodField
 
     class Meta:
         model = Product
         fields = ['id', 'date_created', 'date_updated', 'name', 'category', 'articles']
 
     def get_articles(self, instance):
-
         queryset = instance.articles.filter(active=True)
         serializer = ArticleSerializer(queryset, many=True)
         return serializer.data
@@ -28,7 +31,14 @@ class ArticleSerializer(ModelSerializer):
         fields = ['id', 'date_created', 'date_updated', 'name', 'price', 'product']
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class CategoryListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ['id', 'date_created', 'date_updated', 'name']
+
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
 
     products = serializers.SerializerMethodField()
 
--- shop/views.py
+++ shop/views.py
@@ -1,27 +1,52 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerializer
+from rest_framework.response import Response
+
+from rest_framework.decorators import action
+
+from shop.serializers import CategoryListSerializer, CategoryDetailSerializer, \
+    ProductSerializer, ProductDetailSerializer, ArticleSerializer
 
 from shop.models import Category, Product, Article
 
+class MultiSerializerMixin:
 
-class CategoryViewset(ReadOnlyModelViewSet):
+    detail_serializer_class = None
 
-    serializer_class = CategorySerializer
+    def get_serializer_class(self):
+        if self.action == 'retrieve' and self.detail_serializer_class is not None:
+            return self.detail_serializer_class
+        return super().get_serializer_class()
+
+class CategoryViewset(MultiSerializerMixin, ReadOnlyModelViewSet):
+
+    serializer_class = CategoryListSerializer
+    detail_serializer_class = CategoryDetailSerializer
 
     def get_queryset(self):
         return Category.objects.filter(active=True)
 
-class ProductViewset(ReadOnlyModelViewSet):
+    @action(detail=True, methods=['post'])
+    def disable(self, request, pk):
+        self.get_object().disable()
+        return Response()
+
+class ProductViewset(MultiSerializerMixin, ReadOnlyModelViewSet):
 
     serializer_class = ProductSerializer
+    detail_serializer_class = ProductDetailSerializer
 
     def get_queryset(self):
         queryset = Product.objects.filter(active=True)
         category_id = self.request.GET.get('category_id')
-        if category_id is not None:
-            queryset = Product.objects.filter(category_id=category_id)
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
         return queryset
+
+    @action(detail=True, methods=['post'])
+    def disable(self, request, pk):
+        self.get_object().disable()
+        return Response()
 
 class ArticleViewset(ReadOnlyModelViewSet):
 
